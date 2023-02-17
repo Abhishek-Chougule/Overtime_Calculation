43bb6b29b864f43abab9beb170dbc93a3547c4be
Abhishek-Chougule <chouguleabhis@gmail.com>
2023-02-17 17:41:18 +0530

    c

--- overtime_cal/overtime_cal/doctype/employee_checkinout/employee_checkinout.js
+++ overtime_cal/overtime_cal/doctype/employee_checkinout/employee_checkinout.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Abhishek Chougule and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Employee Checkinoutout', {
+frappe.ui.form.on('Employee Checkinout', {
 	before_save: function(frm) {
 		frm.call({
 			method:'calot',
