b2c19ba619f6702692c0a846021dd127b25501c3
Abhishek-Chougule <chouguleabhis@gmail.com>
2023-02-17 17:26:27 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Overtime Cal
+
+Overtime Calculation
+
+#### License
+
+Copyright @2023 - Abhishek Chougule chouguleabhis@gmail.com
---
+++ overtime_cal/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Overtime Cal",
+			"type": "module",
+			"label": _("Overtime Cal")
+		}
+	]
